2fa418320da0181677d75739ef98303639f4a6bc
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2015-05-06 23:30:55 -0300

    Initial Commit (edted)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	// button states: OK and Stop
+	var states = {
+		'OK': {
+			"button": 'btn-success',
+			"icon": 'glyphicon-ok'
+		},
+		'Stop': {
+			"button": 'btn-danger',
+			"icon": 'glyphicon-remove'
+		}
+	};
+
+	/* Some event handlers */
+
+});
+
+/* A function to get the next prime number from the one given */
+function getNextPrimeNumber(num){
+
+}
